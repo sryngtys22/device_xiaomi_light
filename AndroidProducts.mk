c5c539f14b507fc15bd064c01963befcda9e3f5c
sryngtys22 <126160963+sryngtys22@users.noreply.github.com>
2025-07-03 12:50:07 +0700

    Update AndroidProducts.mk
    Test infinity-x

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,12 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_light.mk
+    $(LOCAL_DIR)/infinity_ginkgo.mk \
 
 COMMON_LUNCH_CHOICES := \
     lineage_light-user \
     lineage_light-userdebug \
     lineage_light-eng
+    infinity_light-user \
+    infinity_light-userdebug \
+    infinity_light-eng \
